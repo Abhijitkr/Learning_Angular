4e7dd24b2a6a8e76a82de02f7406d5e702e06bee
abhijitkr <abhijitkr600@gmail.com>
2025-12-01 22:32:05 +0530

    Adds the Modal, also feature of edit and delete function

--- crud-app/src/app/home/home.component.ts
+++ crud-app/src/app/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { UserForm } from '../model/UserForm';
 
-
 @Component({
   selector: 'app-home',
   imports: [ReactiveFormsModule],
@@ -11,14 +10,46 @@ import { UserForm } from '../model/UserForm';
 })
 export class HomeComponent {
   userForm: FormGroup;
+  editForm: FormGroup;
   persons: UserForm[] = [];
+  selectedIndex: number | null = null;
+
   constructor(private fb: FormBuilder){
+
     this.userForm = this.fb.group({
       firstName: [''],
       lastName: [''],
       dob: [''],
       qualification: ['']
-    })
+    });
+
+     this.editForm = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      dob: [''],
+      qualification: ['']
+    });
+  }
+
+  selectForEdit(index: number) {
+    this.selectedIndex = index;
+    this.editForm.patchValue(this.persons[index]);
+  }
+  
+ saveEdit() {
+    if (this.selectedIndex === null) return;
+    this.persons[this.selectedIndex] = this.editForm.value;
+    this.selectedIndex = null;
+    this.editForm.reset();
+  }
+
+  delete(index: number) {
+    if (index < 0 || index >= this.persons.length) return;
+    if (this.selectedIndex === index) {
+      this.selectedIndex = null;
+      this.editForm.reset();
+    }
+    this.persons.splice(index, 1);
   }
 
   submitFrom(){
